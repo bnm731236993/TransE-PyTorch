949e74d6787f6cc19a784427a785e10df64dd4de
bnm731236993 <bnm731236993@outlook.com>
2025-04-08 22:28:58 +0800

    beta

--- model.py
+++ model.py
@@ -12,29 +12,42 @@ class TransE(nn.Module):
         self.device = device
         self.norm = norm
         self.dim = dim
+
+        # 实体嵌入
         self.entities_emb = self._init_enitity_emb()
+        # 关系嵌入
         self.relations_emb = self._init_relation_emb()
+        # 边界排序损失
         self.criterion = nn.MarginRankingLoss(margin=margin, reduction='none')
 
     def _init_enitity_emb(self):
+        # 嵌入层
         entities_emb = nn.Embedding(num_embeddings=self.entity_count + 1,
                                     embedding_dim=self.dim,
                                     padding_idx=self.entity_count)
+        # 初始化
         uniform_range = 6 / np.sqrt(self.dim)
         entities_emb.weight.data.uniform_(-uniform_range, uniform_range)
         return entities_emb
 
     def _init_relation_emb(self):
+        # 嵌入层
         relations_emb = nn.Embedding(num_embeddings=self.relation_count + 1,
                                      embedding_dim=self.dim,
                                      padding_idx=self.relation_count)
+        # 初始化
         uniform_range = 6 / np.sqrt(self.dim)
         relations_emb.weight.data.uniform_(-uniform_range, uniform_range)
         # -1 to avoid nan for OOV vector
-        relations_emb.weight.data[:-1, :].div_(relations_emb.weight.data[:-1, :].norm(p=1, dim=1, keepdim=True))
+        relations_emb.weight.data[:-1, :].div_(
+            relations_emb.weight.data[:-1, :].norm(p=1, dim=1, keepdim=True))
         return relations_emb
 
-    def forward(self, positive_triplets: torch.LongTensor, negative_triplets: torch.LongTensor):
+    def forward(self,
+                # 正三元组
+                positive_triplets: torch.LongTensor,
+                # 负三元组
+                negative_triplets: torch.LongTensor):
         """Return model losses based on the input.
 
         :param positive_triplets: triplets of positives in Bx3 shape (B - batch, 3 - head, relation and tail)
@@ -42,9 +55,11 @@ class TransE(nn.Module):
         :return: tuple of the model loss, positive triplets loss component, negative triples loss component
         """
         # -1 to avoid nan for OOV vector
-        self.entities_emb.weight.data[:-1, :].div_(self.entities_emb.weight.data[:-1, :].norm(p=2, dim=1, keepdim=True))
+        self.entities_emb.weight.data[:-1, :].div_(
+            self.entities_emb.weight.data[:-1, :].norm(p=2, dim=1, keepdim=True))
 
         assert positive_triplets.size()[1] == 3
+        # 计算三元组的距离
         positive_distances = self._distance(positive_triplets)
 
         assert negative_triplets.size()[1] == 3
@@ -70,5 +85,4 @@ class TransE(nn.Module):
         heads = triplets[:, 0]
         relations = triplets[:, 1]
         tails = triplets[:, 2]
-        return (self.entities_emb(heads) + self.relations_emb(relations) - self.entities_emb(tails)).norm(p=self.norm,
-                                                                                                          dim=1)
+        return (self.entities_emb(heads) + self.relations_emb(relations) - self.entities_emb(tails)).norm(p=self.norm, dim=1)
